eff850e7f2da730df77a555b2cef08f319a1ec8b
mirza-z <zaimovic330@gmail.com>
2023-05-06 19:33:24 +0200

    Finished the app

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'> 
+        <h2>About</h2>
+        <p style={{ marginTop: "1rem"}}>
+          This blog app was made 06.05.2023. in React JS.
+        </p>
+    </main>
+  )
+}
+
+export default About
